7c2778eac8a3b3a9e6be6bf2de2c6f9b6f6879a3
dirtydishes <dishes@riseup.net>
2025-10-30 11:02:54 -0400

    Create Xcode project

---
+++ LyricsLab/LyricsLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  LyricsLabApp.swift
+//  LyricsLab
+//
+//  Created by Kellan Drucquer on 10/30/25.
+//
+
+import SwiftUI
+
+@main
+struct LyricsLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LyricsLabTests/LyricsLabTests.swift
@@ -0,0 +1,16 @@
+//
+//  LyricsLabTests.swift
+//  LyricsLabTests
+//
+//  Created by Kellan Drucquer on 10/30/25.
+//
+
+import Testing
+
+struct LyricsLabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
